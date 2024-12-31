9f0c6952c746fe8ef4ecad518caa8190062f4613
SANTIAGO LOPEZ <santiagolopezslz60@gmail.com>
2024-12-31 13:26:36 -0300

    Add project files.

---
+++ LogIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System_Fitness
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+        private void guna2ContainerControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ContainerControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
